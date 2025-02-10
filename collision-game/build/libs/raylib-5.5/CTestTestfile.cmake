0441dd663e3ff401d29b3592468b46d2400dcfba
Medikenji <120372510+Medikenji@users.noreply.github.com>
2025-02-10 10:19:21 +0100

    No more build

--- collision-game/build/libs/raylib-5.5/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nolan/OneDrive - ROC Alfa-college/SotwareDevelopment/C++/Buas-Intake-Opdracht/collision-game/libs/raylib-5.5
-# Build directory: C:/Users/nolan/OneDrive - ROC Alfa-college/SotwareDevelopment/C++/Buas-Intake-Opdracht/collision-game/build/libs/raylib-5.5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
