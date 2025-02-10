913f3233dabf91d39d916b2d4d6db48a8eea990c
Medikenji <120372510+Medikenji@users.noreply.github.com>
2025-02-10 10:18:29 +0100

    Clean slate that actually BUILDS

--- collision-game/main.cpp
+++ collision-game/main.cpp
@@ -1,12 +1,5 @@
 #define RAYGUI_IMPLEMENTATION
-#include "Core.h"
 
-int main(void)
-{
-    Core *core = new Core();
-    core->Run();
-    delete core;
-    return 0;
+int main() {
+  return 0;
 }
-
-// inheretance
