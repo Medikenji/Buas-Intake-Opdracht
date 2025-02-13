e33bdf985fdffc78ce80809cab80ac0dd94719a7
Medikenji <nolanbijmholt@gmail.com>
2025-02-13 18:05:19 +0100

    Most usefull comments EVER written

--- collision-game/main.cpp
+++ collision-game/main.cpp
@@ -2,8 +2,13 @@
 #include "Core.h"
 
 int main() {
+  // Create a new game instance
   Core *game = new Core();
+
+  // Run the game
   game->Run();
+
+  // Delete the game instance
   delete game;
   return 0;
 }
