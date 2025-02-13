2b19401f570c9aeeeac9140783adab4baa3d6879
Medikenji <nolanbijmholt@gmail.com>
2025-02-13 18:22:52 +0100

    Started on player size logic

--- collision-game/headers/Player.h
+++ collision-game/headers/Player.h
@@ -17,14 +17,18 @@ public:
 private:
   // --- Functions --- //
 
-  /// @brief handles the input for the player, to keep the Run function clean.
+  /// @brief handles the input for the player, to keep the Run function clean. (Supports max 2 players)
   void handleInput();
 
+  /// @brief handles the size of the player, has a max size of 100. Exists to keep the Run function clean.
+  void handleSize();
+
   // --- Variables --- //
-  static int splayerNum;
-  float speed;
-  int playerNum;
-  Color playerColor;
+  static int s_playerNum;
+  float _speed;
+  int _scale;
+  int _playerNum;
+  Color _playerColor;
 };
 
 #endif
--- collision-game/sources/Player.cpp
+++ collision-game/sources/Player.cpp
@@ -1,20 +1,20 @@
 #include "Player.h"
 
-int Player::splayerNum = 0;
+int Player::s_playerNum = 0;
 
 Player::Player() {
-  this->playerNum = Player::splayerNum++;
-  this->speed = 15;
-  this->scale.x = 10;
+  this->_playerNum = Player::s_playerNum++;
+  this->_speed = 15;
+  this->_scale = 5;
   this->inBounds = true;
 
   // Set the player color and startpos based on player number
-  if (playerNum == 0) {
+  if (this->_playerNum == 0) {
     this->position = {SCREEN_WIDTH - this->scale.x, SCREEN_HEIGHT - this->scale.x};
-    playerColor = MAGENTA;
+    this->_playerColor = MAGENTA;
   } else {
     this->position = {0 + this->scale.x, 0 + this->scale.x};
-    playerColor = SKYBLUE;
+    _playerColor = SKYBLUE;
   }
 }
 
@@ -23,6 +23,8 @@ Player::~Player() {
 
 void Player::Run(float deltaTime) {
   this->handleInput();
+
+  // Move the player
   this->position.x += this->velocity.x * deltaTime;
   this->position.y += this->velocity.y * deltaTime;
 
@@ -30,35 +32,45 @@ void Player::Run(float deltaTime) {
   this->velocity.x *= std::pow(0.3f, deltaTime);
   this->velocity.y *= std::pow(0.3f, deltaTime);
 
-  DrawCircleLines((int)this->position.x, (int)this->position.y, this->scale.x, playerColor);
+  this->handleSize();
+
+  // Draw the player
+  DrawCircleLines((int)this->position.x, (int)this->position.y, this->scale.x, _playerColor);
 }
 
 void Player::handleInput() {
-  if (playerNum == 0) {
+  if (_playerNum == 0) {
     if (IsKeyDown(KEY_UP)) {
-      this->velocity.y -= this->speed;
+      this->velocity.y -= this->_speed;
     }
     if (IsKeyDown(KEY_DOWN)) {
-      this->velocity.y += this->speed;
+      this->velocity.y += this->_speed;
     }
     if (IsKeyDown(KEY_LEFT)) {
-      this->velocity.x -= this->speed;
+      this->velocity.x -= this->_speed;
     }
     if (IsKeyDown(KEY_RIGHT)) {
-      this->velocity.x += this->speed;
+      this->velocity.x += this->_speed;
     }
   } else {
     if (IsKeyDown(KEY_W)) {
-      this->velocity.y -= this->speed;
+      this->velocity.y -= this->_speed;
     }
     if (IsKeyDown(KEY_S)) {
-      this->velocity.y += this->speed;
+      this->velocity.y += this->_speed;
     }
     if (IsKeyDown(KEY_A)) {
-      this->velocity.x -= this->speed;
+      this->velocity.x -= this->_speed;
     }
     if (IsKeyDown(KEY_D)) {
-      this->velocity.x += this->speed;
+      this->velocity.x += this->_speed;
     }
+  }
+}
+
+void Player::handleSize() {
+  this->scale.x = this->getTotalSpeedInt() * 0.02 + this->_scale;
+  if (this->scale.x > 100) {
+    this->scale.x = 100;
   }
 }
