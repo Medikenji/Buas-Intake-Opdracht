65d95063b2d973980dc38754edc884df0b32fb83
Medikenji <nolanbijmholt@gmail.com>
2025-02-16 12:48:37 +0100

    Starting on dynamic resolution

--- collision-game/configurations/PROGRAM_CONFIG.h
+++ collision-game/configurations/PROGRAM_CONFIG.h
@@ -3,8 +3,8 @@
 
 // a set of variables that change the behaviour of the program
 
-#define SCREEN_WIDTH  1280
-#define SCREEN_HEIGHT 1280
+#define SCREEN_WIDTH  900
+#define SCREEN_HEIGHT 1080
 
 #define MAX_FPS 60
 
@@ -13,5 +13,13 @@
 
 ///@brief changes the maximum framerate of the program to 1000 for performance testing
 #define PERFORMANCE_TEST 0
+
+#if SCREEN_WIDTH > SCREEN_HEIGHT
+#define SCALER     SCREEN_HEIGHT
+#define NON_SCALER SCREEN_WIDTH
+#else
+#define SCALER     SCREEN_WIDTH
+#define NON_SCALER SCREEN_HEIGHT
+#endif
 
 #endif
--- collision-game/sources/GameScene.cpp
+++ collision-game/sources/GameScene.cpp
@@ -2,6 +2,10 @@
 
 GameScene::GameScene() {
   this->addChild(m_GameManager = new GameManager());
+  // position.x = (NON_SCALER - SCALER) / 2;
+  position.y = (NON_SCALER - SCALER) / 2;
+  this->scale = {SCALER,
+                 SCALER};
 }
 
 GameScene::~GameScene() {
@@ -9,5 +13,6 @@ GameScene::~GameScene() {
 }
 
 void GameScene::Run(float deltaTime) {
+  DrawRectangleLines(position.x, position.y, scale.x + 1, scale.y + 1, WHITE);
   this->runChildren(deltaTime);
 }
